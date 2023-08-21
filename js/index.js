6df615224ee5ff3cf0b0f6bd817bde24bafd10be
jungminmin00 <jjungjj0311@gmail.com>
2023-08-22 01:32:15 +0900

    box02완

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ $(function(){
   let windowW = $(window).width();
   console.log(windowW);
   // allData();
-
+  // scrollAll();
   if(windowW >= 1339){
     // nav();
     // subMenu();
@@ -84,6 +84,35 @@ function allData(){
   
 }
 
+function scrollAll(){
+  window.addEventListener("wheel", function(e){
+    e.preventDefault();
+  },{passive : false});
+  var $html = $("html");
+  var page = 1;
+  var lastPage = $(".content").length;
+
+  $html.animate({scrollTop:0},10);
+  $(window).on("wheel", function(e){
+  
+    if($html.is(":animated")) return;
+  
+    if(e.originalEvent.deltaY > 0){
+      if(page== lastPage) return;
+  
+      page++;
+    }else if(e.originalEvent.deltaY < 0){
+      if(page == 1) return;
+  
+      page--;
+    }
+    var posTop = (page-1) * $(window).height();
+  
+    $html.animate({scrollTop : posTop});
+ 
+  });
+
+}
 // 내비게이션
 function nav(){     // top포함
   $('#header nav>ul>li>a').on('click', function(){
